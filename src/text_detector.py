6f2dfe05641fae92f2387cc8ce592eda6cc2c155
fostn <106077105+fostn@users.noreply.github.com>
2023-06-18 22:34:20 +0300

    Update text_detector.py

--- src/text_detector.py
+++ src/text_detector.py
@@ -1,15 +1,11 @@
 import easyocr
 import os
-import gdown
+from google_drive_downloader import GoogleDriveDownloader as gdd
 class TextDetector:
     def __init__(self):
-        #self.download_models()
-        try:
-         self.reader = easyocr.Reader(['en','ar'], download_enabled=False, 
+        self.download_models()
+        self.reader = easyocr.Reader(['en'], download_enabled=False, 
                                      model_storage_directory='models',) 
-        except:
-         exit("Erro cannot found models")
-
     def detect_text(self, image_path):
         # Read text from the image using EasyOCR
         result = self.reader.readtext(image_path)
@@ -24,21 +20,22 @@ class TextDetector:
     def download_models(self):
             # Create the 'models' directory if it doesn't exist
             os.makedirs('models', exist_ok=True)
-
             # Specify the URLs and filenames of the files you want to download
             file_urls = [#
-                ('https://drive.google.com/file/d/1RZjuAomSCKTyrD1L39B8hXAtJmmU9Ple/view?usp=drive_link', 'arabic.pth'),
                 ('https://drive.google.com/file/d/1Q8E5qSOAUblwRKwUmwcMalWI5kaG3FPa/view?usp=drive_link', 'craft_mlt_25k.pth'),
                 ('https://drive.google.com/file/d/1HKFjb0Qv3DSj63bl9X-zXSRdL0HC_PaG/view?usp=drive_link', 'english_g2.pth')
             ]
 
             # Download each file if it doesn't already exist
-            for url, filename in file_urls:
-                file_path = os.path.join('models', filename)
+            for file_url, output_path in file_urls:
+                file_path = os.path.join('models', output_path)
                 if not os.path.exists(file_path):
-                    gdown.download(url, file_path)
+                    file_id = file_url.split('/')[-2]
+                    gdd.download_file_from_google_drive(file_id=file_id, dest_path=file_path)
+
                 else:
                     pass
+
 
 
 
