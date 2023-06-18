efbb09dcf38aa430c1dfb02f0253af8ec3464b89
fostn <106077105+fostn@users.noreply.github.com>
2023-06-18 20:56:29 +0300

    Update text_detector.py

--- src/text_detector.py
+++ src/text_detector.py
@@ -2,12 +2,14 @@ import easyocr
 
 class TextDetector:
     def __init__(self):
+        if not os.path.exists('models'):
+            exit("Please create models folder first")
         # Initialize EasyOCR reader
         try:
          self.reader = easyocr.Reader(['en','ar'], download_enabled=False, 
                                      model_storage_directory='models',) 
         except:
-         exit("Erro cannot found models")
+         exit("Erro cannot found models : arabic.pth , craft_mlt_25k.pth , english_g2.pth")
 
     def detect_text(self, image_path):
         # Read text from the image using EasyOCR
