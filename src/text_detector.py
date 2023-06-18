797e613358777478b1c994e0cc7186f427898e85
fostn <106077105+fostn@users.noreply.github.com>
2023-06-18 14:05:58 +0300

    Create text_detector.py

---
+++ src/text_detector.py
@@ -0,0 +1,20 @@
+import easyocr
+
+class TextDetector:
+    def __init__(self):
+        # Initialize EasyOCR reader
+        self.reader = easyocr.Reader(['en','ar'], download_enabled=False, 
+                                     model_storage_directory='models',) 
+
+    def detect_text(self, image_path):
+        # Read text from the image using EasyOCR
+        result = self.reader.readtext(image_path)
+        
+        # Extract the detected text
+        try:
+            detected_text = ' '.join([text[1] for text in result])
+            return detected_text
+        except:
+            return None
+
+
