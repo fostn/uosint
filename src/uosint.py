b7cb9e94c87d7309fa2dc4e746ceac6d48daa145
fostn <106077105+fostn@users.noreply.github.com>
2023-06-18 14:07:32 +0300

    Update uosint.py

--- src/uosint.py
+++ src/uosint.py
@@ -1,16 +1,19 @@
 from .api import InstagramAPI
 from .chatgpt import Chatgbt
-from.download import Download
+from .download import Download
+from .text_detector import TextDetector
 from tabulate import tabulate
 import re
 import datetime
 import time
+import os
 class Uosint:
 	def __init__(self):
 		self.api = InstagramAPI()
 		self.api.authenticate_user()
 		self.Chatgbt = Chatgbt()
 		self.Download = Download()
+		self.text_detector = TextDetector()
 	def extract_email(self,text):
 		pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
 		matches = re.findall(pattern, str(text).replace(' ',''))
@@ -28,8 +31,6 @@ class Uosint:
 		users = self.api.get_followers(target)
 		if users is None:
 			return
-		if "Account Private" in str(users) :
-			exit("Account Private")
 		
 		for user in users:
 			username = user.get('username')
@@ -46,8 +47,6 @@ class Uosint:
 		users = self.api.get_following(target)
 		if users is None:
 			exit('username not found') 
-		if "Account Private" in str(users) :
-			exit("Account Private")
 		for user in users:
 			username = user.get('username')
 			user_id = user.get('pk')
@@ -77,6 +76,7 @@ class Uosint:
 		['Extracted Emails', self.extract_email(info['biography'])],
 		['Extracted Phone Numbers', self.extract_phone_numbers(info['biography'])],
 		['Extracted Urls', self.extract_website_urls(info['biography'])],
+		
 		]
 
 		print(tabulate(user_table, tablefmt='plain'))
@@ -87,8 +87,6 @@ class Uosint:
 		posts = self.api.get_posts(username)
 		if posts is None:
 			exit('username not found')
-		if "Account Private" in posts :
-			exit("Account Private")
 		max_length = 60
 		max_words = 5
 		user_posts = []
@@ -125,8 +123,6 @@ class Uosint:
 		data = self.api.get_posts(username)
 		if data is None:
 			exit("username not found")
-		if "Account Private" in data :
-			exit("Account Private")
 		user_comments = []
 		for index, comment in enumerate(data, 1):
 			caption = comment.get("caption")
@@ -152,8 +148,6 @@ class Uosint:
 		posts = self.api.get_posts(username)
 		if posts is None:
 			exit("username not found")
-		if "Account Private" in posts :
-			exit("Account Private")
 		for post in posts:
 			rows = []
 			post_id = post.get("pk")
@@ -241,24 +235,19 @@ class Uosint:
 				username = users["username"]
 				info = self.api.get_user_info(username) 
 				bio = info.get("biography")
-				print(bio)
 				print(username,':',self.extract_phone_numbers(str(bio)))
 				time.sleep(1)
 	def find_sensitive_data(self,data):
 		if isinstance(data, str):
 			# Convert single text input to a list
 			data = [data]
-		found_sensitive_data = False
 		for item in data:
 			message = f"if there is sensitive data in this text tell me, only reply yes or no text = '{item}'"
 			response = self.Chatgbt.send_message(message)
 			if "yes" in str(response).lower():
-				print(f"sensitive: {item}")
-				found_sensitive_data = True
+				return True
 			else:
-				print(f"unsensitive : {item}")
-		if not found_sensitive_data:
-			print("No sensitive data found".upper())
+				return False
 	def get_sensitive_comments(self, username):
 		user_comments = []
 		data = self.api.get_posts(username)
@@ -268,7 +257,9 @@ class Uosint:
 			
 		if "Account Private" in data:
 			exit("Account Private")
-		print("searching for sensitive data in user comments...")
+			
+		print("Searching for sensitive data in user comments...")
+		
 		for comment in data:
 			caption = comment.get("caption")
 			if caption:
@@ -277,15 +268,51 @@ class Uosint:
 		
 		if user_comments:
 			table = [
-					["Status",'   ','	',' ',"Comment"]
-				]
-			print(tabulate(table,tablefmt='plain'))
-			self.find_sensitive_data(user_comments)
+				["Sensitive", "Comment"]
+			]
+			
+			for comment in user_comments:
+				sensitive_data_found = self.find_sensitive_data(comment)
+				truncated_comment = comment[:20] + "..." if len(comment) > 20 else comment
+				table.append([str(sensitive_data_found), truncated_comment])
+			print(tabulate(table, tablefmt='plain'))
 		else:
 			print("No comments found")
+
+   
 	def download_posts(self,username):
 		self.Download.posts(username)
   
 	def download_stories(self,username):
 		self.Download.stories(username)
+  
+	def detect(self, username):
+		#self.Download.stories(username)  # Download the user's stories
+		stories_folder = os.path.join("stories", username)
+		if not os.path.exists(stories_folder):
+			print(f"No downloaded stories found for {username}")
+			return
+
+		story_images = os.listdir(stories_folder)
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("Starting detecting...")
+		for image_file in story_images:
+			image_path = os.path.join(stories_folder, image_file)
+			if image_file.endswith(".jpg"):
+				print(f"[detecting] {image_file}")
+				text = self.text_detector.detect_text(image_path)
+				if text:
+					analysis = self.Chatgbt.send_message(f"create description of what's going on here if you cant just reply with None: {text}")
+					truncated_text = text[:50] + "..." if len(text) > 40 else text
+					print("[Extracted text]\n",truncated_text+'\n')
+					print("[Analysis]\n",analysis)
+					extracted_info = self.Chatgbt.send_message(f'if you find any personal in info this text reply with key-value pair for every info else reply with No Extracted info : text = {text}')
+					print("[Extracted info]\n",extracted_info)
+					
+				else:
+					print(f"No detected text: {image_file}")
+			else:
+				pass
+			
+
   
