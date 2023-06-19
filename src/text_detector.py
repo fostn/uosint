f0234fb85dae71ced0d51f84b3d869b308975e48
fostn <106077105+fostn@users.noreply.github.com>
2023-06-19 19:29:26 +0300

    Update text_detector.py

--- src/text_detector.py
+++ src/text_detector.py
@@ -5,14 +5,15 @@ import requests
 import zipfile
 class TextDetector:
     def __init__(self):
+        self.root_folder = os.path.dirname(os.path.abspath(__file__))
+        self.models_folder = os.path.join(self.root_folder, "models")
         self.download_models()
         self.download_arabic_pth()
-        self.reader = easyocr.Reader(['en','ar'], download_enabled=False, 
-                                     model_storage_directory='models',) 
+        self.reader = easyocr.Reader(['en', 'ar'], download_enabled=False, model_storage_directory=self.models_folder)
+
     def detect_text(self, image_path):
         # Read text from the image using EasyOCR
         result = self.reader.readtext(image_path)
-        
         # Extract the detected text
         try:
             detected_text = ' '.join([text[1] for text in result])
@@ -21,29 +22,30 @@ class TextDetector:
             return None
 
     def download_models(self):
-            # Create the 'models' directory if it doesn't exist
-            os.makedirs('models', exist_ok=True)
-            # Specify the URLs and filenames of the files you want to download
-            file_urls = [#
-                ('https://drive.google.com/file/d/1Q8E5qSOAUblwRKwUmwcMalWI5kaG3FPa/view?usp=drive_link', 'craft_mlt_25k.pth'),
-                ('https://drive.google.com/file/d/1HKFjb0Qv3DSj63bl9X-zXSRdL0HC_PaG/view?usp=drive_link', 'english_g2.pth')
-            ]
+        # Create the 'models' directory if it doesn't exist
+        os.makedirs(self.models_folder, exist_ok=True)
+        # Specify the URLs and filenames of the files you want to download
+        file_urls = [
+            ('https://drive.google.com/file/d/1Q8E5qSOAUblwRKwUmwcMalWI5kaG3FPa/view?usp=drive_link',
+             'craft_mlt_25k.pth'),
+            ('https://drive.google.com/file/d/1HKFjb0Qv3DSj63bl9X-zXSRdL0HC_PaG/view?usp=drive_link',
+             'english_g2.pth')
+        ]
 
-            # Download each file if it doesn't already exist
-            for file_url, output_path in file_urls:
-                file_path = os.path.join('models', output_path)
-                if not os.path.exists(file_path):
-                    file_id = file_url.split('/')[-2]
-                    gdd.download_file_from_google_drive(file_id=file_id, dest_path=file_path)
+        # Download each file if it doesn't already exist
+        for file_url, output_path in file_urls:
+            file_path = os.path.join(self.models_folder, output_path)
+            if not os.path.exists(file_path):
+                file_id = file_url.split('/')[-2]
+                gdd.download_file_from_google_drive(file_id=file_id, dest_path=file_path)
 
-                else:
-                    pass
     def download_arabic_pth(self):
-        url = "https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/arabic.zip"
-        zip_destination = "models/arabic.zip"
-        extraction_destination = "models/"
-        if os.path.exists(zip_destination):
+        pth_path = os.path.join(self.models_folder, "arabic.pth")
+        if os.path.exists(pth_path):
             return
+
+        url = "https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/arabic.zip"
+        zip_destination = os.path.join(self.models_folder, "arabic.zip")
 
         response = requests.get(url, stream=True)
         response.raise_for_status()
@@ -56,7 +58,7 @@ class TextDetector:
 
         # Extract the contents of the ZIP file
         with zipfile.ZipFile(zip_destination, "r") as zip_ref:
-            zip_ref.extractall(extraction_destination)
+            zip_ref.extractall(self.models_folder)
 
         # Delete the ZIP file
         os.remove(zip_destination)
