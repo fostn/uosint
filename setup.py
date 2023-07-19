02e2972b361ec591cd0641bf1ec8ac73eeeb7d19
fostn <106077105+fostn@users.noreply.github.com>
2023-07-20 01:24:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         'requests',
         'tabulate',
-        'pycryptodome'
+        'pycryptodome',
         'easyocr',
         "GoogleDriveDownloader"
     ],
